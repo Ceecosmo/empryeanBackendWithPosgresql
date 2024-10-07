c265db7f7e9146760c3f2afa873a55bbb702b72a
Ceecosmo <153816310+Ceecosmo@users.noreply.github.com>
2024-10-07 01:16:46 +0100

    Update settings.py

--- empryeanTech/settings.py
+++ empryeanTech/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 # DEBUG = config("DEBUG", cast=bool)
 #  AGE = config("AGE", cast=int)
 
-ALLOWED_HOSTS = ["127.0.0.1:8000"]
+ALLOWED_HOSTS = ["127.0.0.1,primeafricatechnologiess.onrender.com"]
 
 
 # Application definition
